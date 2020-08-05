6903922a73ebf6ddbc4d7b8fac6fb41c754c53c5
ryancdavey1 <ryancdavey1@gmail.com>
2020-08-05 12:01:54 -0600

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -18,7 +18,12 @@ class PigLatinizer
       if self.is_vowel?(word[0])
         new_words << "#{word}way"
       else
-        new_words << "#{word[1..word.length-1]}#{word[0]}ay"
+        word.split("").each.with_index do |c, index|
+          if self.is_vowel?(c)
+            new_words << "#{word[index..word.length-1]}#{word[0..index-1]}ay"
+            break
+          end
+        end
       end
     end
     new_words.join(" ")
