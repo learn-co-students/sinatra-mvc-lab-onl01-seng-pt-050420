038d6a4f6782e32e3140724199dd962187843698
ryancdavey1 <ryancdavey1@gmail.com>
2020-08-05 12:06:38 -0600

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -3,7 +3,6 @@ class PigLatinizer
   @@vowels = ["a", "e", "i", "o", "u"]
   
   def get_words(text)
-    #@arr = get_words(@text.to_s)
     text.split(" ")
   end
 
@@ -20,7 +19,7 @@ class PigLatinizer
       else
         word.split("").each.with_index do |c, index|
           if self.is_vowel?(c)
-            new_words << "#{word[index..word.length-1]}#{word[0..index-1]}ay"
+            new_words << "#{word[index..word.length]}#{word[0..index-1]}ay"
             break
           end
         end
@@ -28,12 +27,5 @@ class PigLatinizer
     end
     new_words.join(" ")
   end
-  #   self.get_words.each do |word|
-  #     if self.is_vowel?(word[0])
-  #       new_words << word + "way"
-  #     else
-  #       new_words << "#{word[1..word.length-1]}#{word[0]}ay"
-  #     end
-  #   end
-  #   new_words.join(" ")
+
 end
